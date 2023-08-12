47f408ca72a1aa5190c5a2e441a436640d8c06d2
윤주혜 <yunju421@hanmail.net>
2023-08-12 13:35:09 +0900

    Style: 공통 스타일 추가

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -1,21 +1,45 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import './styles/colors.scss';
-
+import common from './constants/commonStyle';
 const GlobalStyles = createGlobalStyle`
   ${reset}
+            -ms-overflow-style: none !important; 
+            scrollbar-width: none !important; 
+            ::-webkit-scrollbar {
+              display: none !important; 
+              width: 0 !important;  
+              height: 0 !important;
+              background: white !important;  
+              -webkit-appearance: none !important;
+              appearance: none !important;
+            }
     a {
           text-decoration : none;
           color : inherit;
     }
     body {
-            font-family : Urbanist,apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell , 'Helvetica Neue', sans-serif;
-            font-size : 10px;
+            height: 100vh;
+            cursor: default;
+            font-family : apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell , 'Helvetica Neue', sans-serif;
+            font-size : 15px;
             background-color : $background;
-            overflow-x : hidden;
+            padding-top: ${({ hasTop }) => (hasTop ? common.topHeaderHeight : 0)};
+            padding-bottom: ${({ hasBottom }) => (hasBottom ? common.bottomHeaderHeight : 0)};
+            overflow: hidden;
+            svg,img,button{
+              cursor: pointer;
+            }
+          }
+    *{
+            box-sizing: border-box;
+    }
+    #root {
+            overflow: hidden;
+            width: 100%;
             display: flex;
             justify-content: center;
-            height: auto;
+            height: 100%;
     }
   `;
 export default GlobalStyles;
