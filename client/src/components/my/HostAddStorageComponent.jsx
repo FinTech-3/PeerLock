4c828d8bde1b5ea92966ecd841557eae0e463480
JongukYang <didwhddnr123@naver.com>
2023-08-17 15:50:23 +0900

    [FE]Feat: 내 공간 추가하기 버튼 추가, StorageDetail 로딩창 추가

---
+++ client/src/components/my/HostAddStorageComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppHeader from '../common/AppHeader';
+
+const HostAddStorageComponent = () => {
+	return (
+		<div>
+			<AppHeader title={'HostAddStorageComponent'} />
+		</div>
+	);
+};
+
+export default HostAddStorageComponent;
